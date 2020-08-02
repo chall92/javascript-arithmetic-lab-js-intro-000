a49e3424fc9765e476f551828d7f1204a877d892
Corbin Hall <hallcorbin92@gmail.com>
2020-08-02 21:53:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function add(a, b) {
-  a += b
+  a + b
 }
 
 function subtract(a, b) {
-  a -= b
+  a - b
 }
 
 function multiple(a, b) {
-  a *= b
+  a * b
 }
 
 function divide(a, b) {
-  a /= b
+  a / b
 }
